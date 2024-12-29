35a2bc97b38a229072c8cb2e8441bea531d2bd21
Saksham Garg <saksham.garg14509@gmail.com>
2024-12-29 20:08:49 +0530

    admin add prod, error 404

--- client/src/pages/notfound404/error404.jsx
+++ client/src/pages/notfound404/error404.jsx
@@ -5,13 +5,19 @@ import errimg from '../../assets/undraw_page-not-found_6wni.svg'
 function error404() {
     return (
         <div className='err-main-con'>
-            
+
             <div className='err-head-con'>
-                <h1>Sorry, Page Not Found!</h1>
+                <h1>Page Not Found!</h1>
             </div>
 
             <div className='err-img-con'>
                 <img src={errimg} alt="" />
+            </div>
+
+            <div className='err-txt-con'>
+                <span>
+                    Check for spelling errors in the URL, or go to the home page.
+                </span>
             </div>
         </div>
     )
