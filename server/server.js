ff48324e0e494cd0a662cc3569f57ab05ae206d8
pavan kalvakota <pavankalvakota@gmail.com>
2025-01-20 00:47:22 +0530

    revert changes to server.js and package.json

--- server/server.js
+++ server/server.js
@@ -65,5 +65,6 @@ app.use('/api/blogs', blogRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000 || process.env.WEBSITES_PORT;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
