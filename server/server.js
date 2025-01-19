d11d9eef022bbcb418efd55b93b5d88e861351c9
pavan kalvakota <pavankalvakota@gmail.com>
2025-01-20 00:27:59 +0530

    update server.js and package.json

--- server/server.js
+++ server/server.js
@@ -65,5 +65,5 @@ app.use('/api/blogs', blogRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000 || process.env.WEBSITES_PORT;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
